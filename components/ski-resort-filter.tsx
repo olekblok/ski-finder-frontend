95752b94e465b0988cc10b1c179b9cee9dc28792
olekblok <blokaleksander@gmail.com>
2024-10-04 13:16:24 +0200

    Add new countries

--- components/ski-resort-filter.tsx
+++ components/ski-resort-filter.tsx
@@ -11,7 +11,7 @@ import { LoadingOverlay } from './loading-overlay'
 import { MountainBackground } from './mountain-background'
 import { Snowflake } from 'lucide-react'
 
-const countries = ['Austria', 'Slovakia', 'Czech Republic', 'Slovenia', 'Poland', 'Lithuania', 'Latvia']
+const countries = ['Austria', 'Slovakia', 'Czech Republic', 'Slovenia', 'Poland', 'Lithuania', 'Latvia', 'Germany', 'Switzerland']
 
 export default function SkiResortFilter() {
   const router = useRouter()
